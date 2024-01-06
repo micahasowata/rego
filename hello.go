0ac4d8f72ca6d28f01aafdb5167c137592b4ca38
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 14:07:27 +0000

    feat: return multiple values from a function

--- hello.go
+++ hello.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-func add(x, y int) int {
-	return x + y
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func main() {
-	fmt.Println(add(42, 13))
+	fmt.Println(swap("world!", "Hello,"))
 }
