4d1349c890129c940b12f9e689b2db38f0b8d108
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 16:30:26 +0000

    feat: use `if` blocks for conditionals

--- hello.go
+++ hello.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+
+	return fmt.Sprint(math.Sqrt(x))
+}
 
 func main() {
-	sum := 1
-
-	for {
-		sum += sum
-		fmt.Println(sum)
-	}
+	fmt.Println(sqrt(2), sqrt(-4))
 }
