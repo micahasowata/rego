e10a42744be710cff9e0842b1a37c3cae48cd7dc
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 16:20:17 +0000

    feat: repeat actions in a normal `for` loop

--- hello.go
+++ hello.go
@@ -2,21 +2,12 @@ package main
 
 import "fmt"
 
-const (
-	Big   = 1 << 100
-	Small = 99 >> 1
-)
-
-func needInt(x int) int {
-	return x*10 + 1
-}
-
-func needFloat(x float64) float64 {
-	return x * 0.1
-}
-
 func main() {
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+	sum := 0
+
+	for i := 0; i < 10; i++ {
+		sum += 1
+	}
+
+	fmt.Println(sum)
 }
