f9f7acdf476c2ebf1d07c00f3eced1cf1fff7e1c
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 14:35:58 +0000

    feat: declare and initialize variable with short assignment

--- hello.go
+++ hello.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-var i, j int = 1, 2
-
 func main() {
+	var i, j int = 1, 2
+	k := 3
 	c, python, java := true, false, "no!"
-	fmt.Println(i, c, python, java)
+	fmt.Println(i, j, k, c, python, java)
 }
