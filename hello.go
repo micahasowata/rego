14c8ae05fb1930ef364ee809371c08c9476fc3fe
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 17:00:38 +0000

    feat: implementing a square root function

--- hello.go
+++ hello.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g \n", v, lim)
+func Sqrt(x float64) float64 {
+	z := 0.0
+	for z < x {
+		z -= (z*z - x) / (2 * z)
 	}
-	return lim
+	return z
 }
 
 func main() {
-	fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+	fmt.Println(Sqrt(4))
 }
