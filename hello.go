672b23ed1df711a224c5d04a2ae226dd0813a006
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 14:52:33 +0000

    feat: print zero values of some data types

--- hello.go
+++ hello.go
@@ -1,18 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"math/cmplx"
-)
-
-var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
-)
+import "fmt"
 
 func main() {
-	fmt.Printf("Type: %T, Value:%v\n", ToBe, ToBe)
-	fmt.Printf("Type: %T, Value:%v\n", MaxInt, MaxInt)
-	fmt.Printf("Type: %T, Value:%v\n", z, z)
+	var i int
+	var f float64
+	var b bool
+	var s string
+
+	fmt.Printf("%v, %v, %v, %q \n", i, f, b, s)
 }
