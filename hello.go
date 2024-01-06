c7503aac8e789d167247cbcee7afe60046fe1da6
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 13:41:18 +0000

    feat: use factored imports to oranize multiple imports

--- hello.go
+++ hello.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math"
 )
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
