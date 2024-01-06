1105272761ac918b02817f87023fe71163081a4f
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 14:48:36 +0000

    feat: use and factor multiple variables of different types

--- hello.go
+++ hello.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	var i, j int = 1, 2
-	k := 3
-	c, python, java := true, false, "no!"
-	fmt.Println(i, j, k, c, python, java)
+	fmt.Printf("Type: %T, Value:%v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T, Value:%v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T, Value:%v\n", z, z)
 }
