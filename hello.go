c718155923659d3f5e78c19595699c4c55cbb66c
Micah Asowata <andrewgary963@gmail.com>
2024-01-06 14:13:12 +0000

    feat: use function with named return values and naked return

--- hello.go
+++ hello.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 )
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
-	fmt.Println(swap("world!", "Hello,"))
+	fmt.Println(split(14))
 }
