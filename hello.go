79a7b71fe15c4367ca8ae983c7fb701b5c639a17
Micah Asowata <andrewgary963@gmail.com>
2024-01-07 17:29:37 +0000

    feat: defer function execution

--- hello.go
+++ hello.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
 func main() {
-	fmt.Print("Go is running on: ")
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Linux")
-	case "darwin":
-		fmt.Println("OS X")
-	default:
-		fmt.Println(os)
+	defer fmt.Println("World!")
 
-	}
+	fmt.Print("Hello, ")
 }
