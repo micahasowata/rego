fccb8de3bc21de8d9c014f0de84a5f2f21cc37a1
Micah Asowata <andrewgary963@gmail.com>
2024-01-10 15:36:23 +0000

    feat: create slices

--- hello.go
+++ hello.go
@@ -3,13 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var msg [2]string
-	msg[0] = "Hello,"
-	msg[1] = "World!"
-
-	fmt.Println(msg[0], msg[1])
-	fmt.Println(msg)
-
 	primes := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(primes)
+
+	s := primes[1:4]
+	fmt.Println(s)
 }
