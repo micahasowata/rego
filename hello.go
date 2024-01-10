8159aa683b92ec27917e53c4adc18d7a7b4635a1
Micah Asowata <andrewgary963@gmail.com>
2024-01-10 17:12:02 +0000

    feat: make a slice. literally.

--- hello.go
+++ hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
-	fmt.Println(s, len(s), cap(s))
-	if s == nil {
-		fmt.Println("nil!")
-	}
+	a := make([]int, 5)
+	fmt.Println(a, len(a), cap(a))
+
+	b := make([]int, 0, 5)
+	fmt.Println(b, len(b), cap(b))
 }
