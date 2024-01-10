4b38f1e585f7f7edd580a4fbb31e7568aec9e92b
Micah Asowata <andrewgary963@gmail.com>
2024-01-10 16:40:07 +0000

    feat: use slice defaults

--- hello.go
+++ hello.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	q := []string{"Bo", "Tori", "Gabriella"}
+	q := []string{"Charles", "Olsen", "Rose", "Sutterland", "Bo", "Tori", "Gabriella"}
+	fmt.Println(q)
+
+	q = q[1:4]
+	fmt.Println(q)
+
+	q = q[:2]
+	fmt.Println(q)
+
+	q = q[1:]
 	fmt.Println(q)
 }
