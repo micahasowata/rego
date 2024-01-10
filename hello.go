206508edf586755246968a64c4e235a5be85db75
Micah Asowata <andrewgary963@gmail.com>
2024-01-10 16:05:41 +0000

    feat: slice literals

--- hello.go
+++ hello.go
@@ -3,21 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	names := [4]string{
-		"John",
-		"Paul",
-		"George",
-		"Ringo",
-	}
-
-	fmt.Println(names)
-
-	a := names[0:2]
-	b := names[1:3]
-
-	fmt.Println(a, b)
-
-	b[0] = "XXX"
-	fmt.Println(a, b)
-	fmt.Println(names)
+	q := []string{"Bo", "Tori", "Gabriella"}
+	fmt.Println(q)
 }
