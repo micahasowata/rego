dbd92e6b2c500ea57d56310dd2b0faeb72074ba4
Micah Asowata <andrewgary963@gmail.com>
2024-01-10 15:55:11 +0000

    feat: use slices to modify arrays

--- hello.go
+++ hello.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
 
-	s := primes[1:4]
-	fmt.Println(s)
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
