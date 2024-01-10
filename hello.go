71925d0edcdd72e991ca1273925563d2be402e2b
Micah Asowata <andrewgary963@gmail.com>
2024-01-10 17:03:56 +0000

    feat: find zero value of a slic

--- hello.go
+++ hello.go
@@ -2,20 +2,10 @@ package main
 
 import "fmt"
 
-func printSlice(s []string) {
-	fmt.Printf("len=%d, cap=%d %v\n", len(s), cap(s), s)
-}
-
 func main() {
-	q := []string{"Charles", "Olsen", "Rose", "Sutterland", "Bo", "Tori", "Gabriella"}
-	printSlice(q)
-
-	q = q[1:4]
-	printSlice(q)
-
-	q = q[:2]
-	printSlice(q)
-
-	q = q[1:]
-	printSlice(q)
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	if s == nil {
+		fmt.Println("nil!")
+	}
 }
