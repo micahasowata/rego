957c60ac0dd764ad034bb3fac5026506a47f7e75
Micah Asowata <andrewgary963@gmail.com>
2024-01-14 18:13:42 +0000

    feat: update `Organiser` and plan a course of action

--- organiser.go
+++ organiser.go
@@ -1,7 +1,8 @@
 package main
 
 type Organiser struct {
-	Path string
+	Path      string
+	UseGlobal bool
 }
 
 // NewOrganiser is the constructor function for the organiser struct
@@ -13,4 +14,9 @@ func NewOrganiser(path string) *Organiser {
 
 // Run is the main worker function for organiser
 func (o *Organiser) Run() {
+	// Confirm if the path is a valid directory
+	// Check the read/write permissions on each file in the directory
+	// Check the file extensions
+	// Based on UseGlobal move files to either root based paths or create directories in the CWD
+	// ("jpg, jpeg, png" -> "Images", ".txt, .pdf" -> "Documents")
 }
