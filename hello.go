a0e514c7067a6de924952abeac97f3c8fe9d9d5d
Micah Asowata <andrewgary963@gmail.com>
2024-01-14 17:48:48 +0000

    feat: manipulate map values, create-read-update-delete

--- hello.go
+++ hello.go
@@ -2,19 +2,18 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	Lat, Long float64
-}
-
-var m = map[string]Vertex{
-	"Bell Labs": {
-		40.68433, -74.39967,
-	},
-	"Google": {
-		37.42202, -122.08408,
-	},
-}
-
 func main() {
-	fmt.Println(m)
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
