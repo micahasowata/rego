dc4a65863db6516b582334991244221dc2aa7863
Micah Asowata <andrewgary963@gmail.com>
2024-01-14 17:41:21 +0000

    feat: create map literals

--- hello.go
+++ hello.go
@@ -6,14 +6,15 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
+var m = map[string]Vertex{
+	"Bell Labs": {
+		40.68433, -74.39967,
+	},
+	"Google": {
+		37.42202, -122.08408,
+	},
+}
 
 func main() {
-	m := make(map[string]Vertex)
-
-	m["Bell Labs"] = Vertex{
-		40.68433, -74.39967,
-	}
-
-	fmt.Println(m["Bell Labs"])
+	fmt.Println(m)
 }
