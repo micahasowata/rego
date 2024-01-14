e2c727dc979ad7a664ce0ca34bd0b3764ec0bda1
Micah Asowata <andrewgary963@gmail.com>
2024-01-14 18:03:30 +0000

    feat: create constructor function for `Organiser`

--- organiser.go
+++ organiser.go
@@ -1,1 +1,12 @@
 package main
+
+type Organiser struct {
+	Path string
+}
+
+// NewOrganiser is the constructor function for the organiser struct
+func NewOrganiser(path string) *Organiser {
+	return &Organiser{
+		Path: path,
+	}
+}
