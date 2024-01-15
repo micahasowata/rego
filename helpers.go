caf1b5a98179457d899eb6439e13c348c18c0b27
Micah Asowata <andrewgary963@gmail.com>
2024-01-15 17:08:50 +0000

    refactor: move the helper function to a separate file for readability

---
+++ helpers.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func moveFile(source, destination string) (err error) {
+	src, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+	fi, err := src.Stat()
+	if err != nil {
+		return err
+	}
+	flag := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	perm := fi.Mode() & os.ModePerm
+	dst, err := os.OpenFile(destination, flag, perm)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		dst.Close()
+		os.Remove(destination)
+		return err
+	}
+	err = dst.Close()
+	if err != nil {
+		return err
+	}
+	err = src.Close()
+	if err != nil {
+		return err
+	}
+	err = os.Remove(source)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- organiser.go
+++ organiser.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -42,44 +41,6 @@ func NewOrganiser(path string, useGlobal bool) *Organiser {
 		Path:      path,
 		UseGlobal: useGlobal,
 	}
-}
-
-func moveFile(source, destination string) (err error) {
-	src, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer src.Close()
-	fi, err := src.Stat()
-	if err != nil {
-		return err
-	}
-	flag := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
-	perm := fi.Mode() & os.ModePerm
-	dst, err := os.OpenFile(destination, flag, perm)
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
-	_, err = io.Copy(dst, src)
-	if err != nil {
-		dst.Close()
-		os.Remove(destination)
-		return err
-	}
-	err = dst.Close()
-	if err != nil {
-		return err
-	}
-	err = src.Close()
-	if err != nil {
-		return err
-	}
-	err = os.Remove(source)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 // Run is the main worker function for organiser
