f835e6d0ccf442b323f9edb3a918c30539c3671f
Micah Asowata <andrewgary963@gmail.com>
2024-01-15 22:12:22 +0000

    feat: create a custom `ft` package to ease changes

---
+++ ft/ft.go
@@ -0,0 +1,61 @@
+package ft
+
+import (
+	"fmt"
+	"strings"
+)
+
+var fileExtensions = map[string]string{
+	".mp3":  "Audio",
+	".wav":  "Audio",
+	".aac":  "Audio",
+	".flac": "Audio",
+	".m4a":  "Audio",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".mkv":  "Video",
+	".mov":  "Video",
+	".wmv":  "Video",
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".gif":  "Images",
+	".bmp":  "Images",
+	".tiff": "Images",
+	".tif":  "Images",
+	".pdf":  "Documents",
+	".docx": "Documents",
+	".xlsx": "Documents",
+	".pptx": "Documents",
+	".txt":  "Documents",
+	".mobi": "Documents",
+	".epub": "Documents",
+	".zip":  "Archives",
+	".rar":  "Archives",
+	".7z":   "Archives",
+	".tar":  "Archives",
+	".gz":   "Archives",
+	".exe":  "Executables",
+	".app":  "Executables",
+	".dmg":  "Executables",
+	".bin":  "Executables",
+	".iso":  "Executables",
+	".msi":  "Executables",
+	".deb":  "Executables",
+	".csv":  "Data",
+	".json": "Data",
+	".xml":  "Data",
+}
+
+func GetFileCategory(extension string) (string, error) {
+	if !strings.HasPrefix(extension, ".") {
+		extension = fmt.Sprintf(".%s", extension)
+	}
+
+	category, found := fileExtensions[extension]
+	if !found {
+		return "", fmt.Errorf("%s not found ", extension)
+	}
+
+	return category, nil
+}
