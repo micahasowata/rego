97766d30c82907bf472c583c50559ffefe743cf2
Micah Asowata <andrewgary963@gmail.com>
2024-01-16 09:12:45 +0000

    test: ensure `createPath` returns properly formatted paths

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/spobly/rego
 
 go 1.21.5
 
-require github.com/mitchellh/go-homedir v1.1.0
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- helper/path_helper_test.go
+++ helper/path_helper_test.go
@@ -1,7 +1,20 @@
 package helper_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/spobly/rego/helper"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCreatePaths(t *testing.T) {
+	base := "/x/y"
+	fc := "Data"
+	filename := "quotes.csv"
 
+	dp, sp := helper.CreatePaths(base, fc, filename)
+
+	assert.Equal(t, dp, fmt.Sprintf("%s/%s/%s", base, fc, filename))
+	assert.Equal(t, sp, fmt.Sprintf("%s/%s", base, fc))
 }
