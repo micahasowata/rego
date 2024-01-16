41820d29267746c16b8524c9af63f4ed45b3c4c4
Micah Asowata <andrewgary963@gmail.com>
2024-01-16 10:09:45 +0000

    test: create a set up function to create temporary directories and files

--- helper/fs_helper_test.go
+++ helper/fs_helper_test.go
@@ -1,11 +1,27 @@
 package helper_test
 
-import "testing"
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+var (
+	source string
+	file   string
+	stat   string
+)
 
 func setUp() {
 	// create a source directory
+	source, _ = os.MkdirTemp("", "source")
 	// create a sample file in that directory
+	file = filepath.Join(source, "sample.txt")
+	_ = os.WriteFile(file, []byte{1, 2}, fs.ModePerm)
+
 	// create a stat directory
+	stat, _ = os.MkdirTemp("", "stat")
 }
 
 func tearDown() {
