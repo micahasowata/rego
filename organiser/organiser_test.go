ba2b81ed5156b2f8b7e235aaf9d5c8b4e80efb6f
Micah Asowata <andrewgary963@gmail.com>
2024-01-16 16:01:30 +0000

    test: ensure that the dot results in the current working directory been the file path

--- organiser/organiser_test.go
+++ organiser/organiser_test.go
@@ -1,1 +1,25 @@
 package organiser_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/spobly/rego/organiser"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewWithDot(t *testing.T) {
+	// arrange
+	p := &organiser.Organiser{}
+	wd, err := os.Getwd()
+	require.NoError(t, err)
+
+	// act
+	o, err := organiser.New(".", false)
+
+	// assert
+	require.NoError(t, err)
+	require.IsType(t, o, p)
+	require.Equal(t, o.Path, wd)
+	require.Equal(t, o.UseGlobal, false)
+}
