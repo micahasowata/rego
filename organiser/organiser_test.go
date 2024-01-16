14eee65463c77ac115ce8b501d4a14abd3cd8ce3
Micah Asowata <andrewgary963@gmail.com>
2024-01-16 16:19:23 +0000

    test: ensure proper behavior when path is provided

--- organiser/organiser_test.go
+++ organiser/organiser_test.go
@@ -2,8 +2,10 @@ package organiser_test
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spobly/rego/organiser"
 	"github.com/stretchr/testify/require"
 )
@@ -22,4 +24,17 @@ func TestNewWithDot(t *testing.T) {
 	require.IsType(t, o, p)
 	require.Equal(t, o.Path, wd)
 	require.Equal(t, o.UseGlobal, false)
+}
+
+func TestNewWithAPath(t *testing.T) {
+	p := "Documents"
+	hd, err := homedir.Dir()
+	require.NoError(t, err)
+
+	o, err := organiser.New(p, false)
+
+	require.NoError(t, err)
+	require.Contains(t, o.Path, hd)
+	require.Contains(t, o.Path, p)
+	require.Equal(t, o.Path, filepath.Join(hd, p))
 }
