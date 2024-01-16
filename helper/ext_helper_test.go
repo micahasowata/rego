d09aee49076351d7e4c4e6ef15733e245c522a94
Micah Asowata <andrewgary963@gmail.com>
2024-01-16 09:33:43 +0000

    test: ensure that valid available extensions are properly categorised

--- helper/ext_helper_test.go
+++ helper/ext_helper_test.go
@@ -1,1 +1,19 @@
 package helper_test
+
+import (
+	"testing"
+
+	"github.com/spobly/rego/helper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidExtension(t *testing.T) {
+	ext := ".png"
+	want := "Images"
+
+	got, err := helper.GetFileCategory(ext)
+
+	require.NoError(t, err)
+	assert.Equal(t, want, got, "file category should be Images")
+}
